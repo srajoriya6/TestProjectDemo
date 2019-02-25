d08924f9bf4a2c108e72a17f18f1f3eacc01de37
srajoriya6 <srajoriya6@gmail.com>
2019-02-25 22:42:26 +0530

    Update HelloGit.java

--- TestProjectDemo/src/main/java/com/raystech/proj0/gittry/HelloGit.java
+++ TestProjectDemo/src/main/java/com/raystech/proj0/gittry/HelloGit.java
@@ -1,5 +1,12 @@
 package com.raystech.proj0.gittry;
 
 public class HelloGit {
+private String name;
+  public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 }
